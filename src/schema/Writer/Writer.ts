58017a0c0a3e82b5b5fb4e14f125924b72870c82
Marko Jovanovic <marko.jovanovic2401@gmail.com>
2021-04-24 01:42:11 +0200

    Writer ID should always be defined

--- src/schema/Writer/Writer.ts
+++ src/schema/Writer/Writer.ts
@@ -3,7 +3,7 @@ import { objectType } from 'nexus';
 const Writer = objectType({
   name: 'Writer',
   definition(t) {
-    t.nullable.int('id'),
+    t.nonNull.int('id'),
     t.nonNull.string('name'),
     t.nonNull.string('about'),
     t.nonNull.string('imgUrl')
